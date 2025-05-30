d3a73bc56df80727e6d992722bf200400e09838f
Lucas <Lukinhasx248@gmail.com>
2025-05-30 09:45:42 -0300

    fiz as coisas da aula

---
+++ react/casalapp/app/App.js
@@ -0,0 +1,32 @@
+import {  StyleSheet } from 'react-native';
+
+import SignUp from './screens/user/Signup';
+import Login from './screens/user/Login';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="SignUp" component={SignUp} options={{ title: 'Crie sua conta' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  scroll: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  container: {
+    padding: 20,
+    gap: 20,
+  },
+});
